f4f71848aba7e52a449846bdec405fe1c33e9480
shan-max <58596614+shan-max@users.noreply.github.com>
2025-10-15 16:14:07 +0530

    Add files via upload

---
+++ new_project/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Currency;{
+
+    public static void main(String[] args){
+        /*convert currency from one to another*/
+        int i = dollar;
+        int j = rupee;
+        if (i = 88.73){
+            System.out.println("print this is doller: " + i);
+    
+        if (j = 1){
+            System.out.pintln("print this is rupee: " + j);
+            }
+            one dollar = 88.73;
+            Ten dollar = i * 10;
+            System.out.println("this is result of ten dollers: " + (i *10) ); 
+
+
+            }
+        }
+    }
+    
+
